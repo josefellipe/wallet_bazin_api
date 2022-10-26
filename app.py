fc9292797e135538afaeb4dead263144a1fde8f5
josefellipe <joseserqueira@id.uff.br>
2022-10-26 14:30:58 -0300

    Criando o Procfile para o heroku

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from Controller.investidor import investidor_blueprint
 from Controller.acao import acao_blueprint
 from flask_cors import CORS
+import os
 
 
 app = Flask(__name__)
@@ -12,5 +13,9 @@ app.register_blueprint(investidor_blueprint)
 app.register_blueprint(acao_blueprint)
 
 
+def main():
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 if __name__ == '__main__':
-    app.run()
+    main()
